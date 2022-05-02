39192d470f88a0e0fd7756801d7c726ba5f3ec23
Duy Le <duy.leduc8@gmail.com>
2022-05-02 23:56:03 +0700

    update category schema, currency schema

---
+++ main/schemas/dump/category.py
@@ -0,0 +1,10 @@
+from marshmallow import fields
+
+from main.schemas.base import BaseSchema
+
+
+class DumpCategorySchema(BaseSchema):
+    id = fields.Integer()
+    name = fields.String()
+    created = fields.DateTime()
+    updated = fields.DateTime()
